6435c633bcfb0950676499011f928e67bbf5a4af
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-07-18 03:34:05 +0100

    contrib/check_keys.sh: Fix some small shell scripting typos

--- contrib/check_keys.sh
+++ contrib/check_keys.sh
@@ -4,14 +4,14 @@
 KEYS_HOST="https://keys.hackery.site/"
 KEYS_USER="$1"
 
-fetched_keys=`curl -q "$KEYS_HOST/keys/$KEYS_USER"`
+fetched_keys=`curl -s "$KEYS_HOST/keys/$KEYS_USER"`
 if [ $? == 0 ]; then
   echo $fetched_keys
 
   authorized_keys_file="~$KEYS_USER/.ssh/authorized_keys"
   if [ -f "$authorized_keys_file" ]; then
-    regular_authorized_keys=`sed '/.*###### SSH-KEY-AUTHORITY SECTION ######.*/{s///;q;}' < authorized_keys_file`
-    
+    regular_authorized_keys=`sed '/.*###### SSH-KEY-AUTHORITY SECTION ######.*/{s///;q;}' < "$authorized_keys_file"`
+
     (echo $regular_authorized_keys; echo '###### SSH-KEY-AUTHORITY SECTION ######
 # PLEASE DO NOT EDIT UNDER THIS SECTION
 # IT WILL BE WIPED BY SSH-KEY-AUTHORITY'; echo $fetched_keys) > authorized_keys_file
