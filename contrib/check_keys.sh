3bc9b2b30714da4804fc5b096e039c5c0f7c0c46
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-07-25 11:39:41 +0100

    contrib/check_keys: Be a little more defensive, fix some typos

--- contrib/check_keys.sh
+++ contrib/check_keys.sh
@@ -6,16 +6,15 @@ eval LOCKBOX_CONFIG_FILE="$LOCKBOX_CONFIG_FILE"
 
 # If the user doesn't have a lockbox configuration, we shouldn't do anything
 if [ -f "$LOCKBOX_CONFIG_FILE" ]; then
-  read -d "\n" keys_host keys_user keys_access < "$LOCKBOX_CONFIG_FILE"
+  read -r -d "\n" keys_host keys_user keys_access < "$LOCKBOX_CONFIG_FILE"
 
-  curl_command="-s"
   if [ -z "$keys_access" ]; then
     fetched_keys=$(curl -s "$keys_host/keys/$keys_user")
   else
     fetched_keys=$(curl -s -H "Authorization: Bearer $keys_access" "$keys_host/keys/$keys_user")
   fi
 
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     echo "$fetched_keys"
 
     authorized_keys_file="~$keys_user/.ssh/authorized_keys"
@@ -26,7 +25,7 @@ if [ -f "$LOCKBOX_CONFIG_FILE" ]; then
       (echo "$regular_authorized_keys"; echo '### LOCKBOX SECTION
 # Please do not edit under this section, it is
 # automatically generated and may be wiped
-# at any time.'; echo "$fetched_keys") > $authorized_keys_file
+# at any time.'; echo "$fetched_keys") > "$authorized_keys_file"
     fi
   else
     >&2 echo "An error occurred while fetching $1's keys from $keys_host."
