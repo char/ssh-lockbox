ca3b8e751b976f92bc469e34f3238aaf39fb74d2
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-07-25 11:38:29 +0100

    contrib/check_keys: Read from ~/.config/lockbox.conf to get a host and user

--- contrib/check_keys.sh
+++ contrib/check_keys.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env sh
 # AuthorizedKeysCommand check_keys.sh "%u"
 
-KEYS_HOST="https://keys.hackery.site/"
-KEYS_USER="$1"
+LOCKBOX_CONFIG_FILE="~$1/.config/lockbox.conf"
+eval LOCKBOX_CONFIG_FILE="$LOCKBOX_CONFIG_FILE"
 
-fetched_keys=`curl -s "$KEYS_HOST/keys/$KEYS_USER"`
-if [ $? == 0 ]; then
-  echo $fetched_keys
+# If the user doesn't have a lockbox configuration, we shouldn't do anything
+if [ -f "$LOCKBOX_CONFIG_FILE" ]; then
+  read -d "\n" keys_host keys_user keys_access < "$LOCKBOX_CONFIG_FILE"
 
-  authorized_keys_file="~$KEYS_USER/.ssh/authorized_keys"
-  if [ -f "$authorized_keys_file" ]; then
-    regular_authorized_keys=`sed '/.*### LOCKBOX SECTION.*/{s///;q;}' < "$authorized_keys_file"`
+  curl_command="-s"
+  if [ -z "$keys_access" ]; then
+    fetched_keys=$(curl -s "$keys_host/keys/$keys_user")
+  else
+    fetched_keys=$(curl -s -H "Authorization: Bearer $keys_access" "$keys_host/keys/$keys_user")
+  fi
 
-    (echo $regular_authorized_keys; echo '### LOCKBOX SECTION
+  if [ $? == 0 ]; then
+    echo "$fetched_keys"
+
+    authorized_keys_file="~$KEYS_USER/.ssh/authorized_keys"
+    eval authorized_keys_file="$authorized_keys_file"
+    if [ -f "$authorized_keys_file" ]; then
+      regular_authorized_keys=$(sed '/.*### LOCKBOX SECTION.*/{s///;q;}' < "$authorized_keys_file")
+
+      (echo "$regular_authorized_keys"; echo '### LOCKBOX SECTION
 # Please do not edit under this section, it is
 # automatically generated and may be wiped
-# at any time.'; echo $fetched_keys) > authorized_keys_file
+# at any time.'; echo "$fetched_keys") > $authorized_keys_file
+    fi
+  else
+    >&2 echo "An error occurred while fetching $1's keys from $keys_host."
   fi
-else
-  >&2 echo "An error occurred while fetching $KEYS_USER's keys from $KEYS_HOST."
 fi
